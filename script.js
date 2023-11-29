30930fc854c88776ed906dfa74f04800c2ebf5fa
Mikita Burak <mikita.burak@revobyte.it>
2023-11-29 13:25:37 +0100

    add projekt

---
+++ script.js
@@ -0,0 +1,53 @@
+import { Column } from "./column.js";
+
+
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+
+
+const FONT_SIZE = 16;
+let columns = [];
+let columnsCount = 0;
+
+
+initCanvasSize();
+
+initColumns()
+
+
+function initCanvasSize() {
+    canvas.width = document.documentElement.clientWidth;
+    canvas.height = document.documentElement.clientHeight;
+  }
+
+  function initColumns() {
+    columnsCount = canvas.width / FONT_SIZE;
+    columns = []
+    for (let i = 0; i < columnsCount; i++) {
+      columns.push(new Column(i * FONT_SIZE, FONT_SIZE, canvas.height, context));
+    }
+    console.log(columns)
+  }
+
+context.font = `bold ${FONT_SIZE}px monospace`;
+
+
+
+const column = new Column(100, FONT_SIZE, canvas.height, context)
+
+
+function animate(){
+    context.fillStyle = 'rgba(0, 0, 0, 0.05)';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    context.fillStyle = "green"
+    columns.forEach(e => {
+        e.drawSymbol()
+    });
+  
+
+    setTimeout(()=> requestAnimationFrame(animate), 50)
+} 
+
+animate()
